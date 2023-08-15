ba89dbb00d38607f18743d1dca6b39ce7b09005f
NANIBOT <136620840+Devibot1@users.noreply.github.com>
2023-08-15 03:32:34 -0600

    Update grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -5,11 +5,11 @@ throw false
 }
 let vn = './media/Invocar.mp3'
 let pesan = args.join` `
-let oi = `*AnxsBot* ${pesan}`
+let oi = `*PACHON DOMINA* ${pesan}`
 let teks = `*MALDITAS BASURAS, LLENEN ALV*\n\n ${oi}\n\n💩 ➢ 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂\n`
 for (let mem of participants) {
-teks += `🎯 @${mem.id.split('@')[0]}\n`}
-teks += `*🐾AnxsBot*\n\n*😼♥️*`
+teks += `💩 @${mem.id.split('@')[0]}\n`}
+teks += `*⚡SÍGUEME EN IG @PACHON_FF⚡*\n\n*😎*`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 conn.sendFile(m.chat, vn, 'Invocar.mp3', null, m, true, { type: 'audioMessage', ptt: true, sendEphemeral: true })
 }
